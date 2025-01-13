356efeaf7389720639e90f1af27f815f5df9640b
harsh-jagtap-josh <harsh.jagtap@joshsoftware.com>
2025-01-13 18:56:56 +0530

    minor changes

--- Day 1 Assessment/areaOfCircle.go
+++ Day 1 Assessment/areaOfCircle.go
@@ -16,14 +16,19 @@ func calcAreaOfCircle(radius float64) float64 {
 	ratio := math.Pow(10, 2)
 	return math.Round(float64(area)*ratio) / ratio
 
+	// s := fmt.Sprintf("%.2f", area)
+
+	// return s
 }
 
 func main() {
 	var radius float64
 
+	fmt.Print("Enter the Radius: ")
 	fmt.Scan(&radius)
-
 	area := calcAreaOfCircle(radius)
 
-	fmt.Print("Area of circle is: ", area)
+	fmt.Println("Area of circle is: ", area)
+
+	// fmt.Println("Area of circle is: ", "%.2f", area)
 }
