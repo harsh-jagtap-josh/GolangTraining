c664394e1034783be82fc53388e3931308abb3af
harsh-jagtap-josh <harsh.jagtap@joshsoftware.com>
2025-01-17 20:05:31 +0530

    add practice code for errors package, wrapping in errors, Is and As functions in errors

--- Day 5 Assessment/Practice/errors.go
+++ Day 5 Assessment/Practice/errors.go
@@ -5,24 +5,51 @@ import (
 	"fmt"
 )
 
-func div(a, b int) (int, error) {
-	if b == 0 {
-		return 0, errors.New("Denominator cannot be zero")
+func f(arg int) (int, error) {
+	if arg == 42 {
+
+		return -1, errors.New("can't work with 42")
 	}
 
-	return a / b, nil
+	return arg + 3, nil
+}
+
+var ErrOutOfTea = fmt.Errorf("no more tea available")
+var ErrPower = fmt.Errorf("can't boil water")
+
+func makeTea(arg int) error {
+	if arg == 2 {
+		return ErrOutOfTea
+	} else if arg == 4 {
+
+		return fmt.Errorf("making tea: %w", ErrPower)
+	}
+	return nil
 }
 
 func main() {
+	for _, i := range []int{7, 42} {
 
-	a, b := 10, 0
-
-	val, err := div(a, b)
-
-	if err != nil {
-		fmt.Println("Error Occured: ", err)
-	} else {
-		fmt.Println("Answer is: ", val)
+		if r, e := f(i); e != nil {
+			fmt.Println("f failed:", e)
+		} else {
+			fmt.Println("f worked:", r)
+		}
 	}
 
+	for i := range 5 {
+		if err := makeTea(i); err != nil {
+
+			if errors.Is(err, ErrOutOfTea) { // here we are checking if err in any has the same value or wraps the ErrOutOfTea error in error tree
+				fmt.Println("We should buy new tea!")
+			} else if errors.Is(err, ErrPower) { // we see that in func makeTea where arg == 4, the error returned wraps the ErrPower, hence the errors.Is function will be true if we pass both these...
+				fmt.Println("Now it is dark.")
+			} else {
+				fmt.Printf("unknown error: %s\n", err)
+			}
+			continue
+		}
+
+		fmt.Println("Tea is ready!")
+	}
 }
