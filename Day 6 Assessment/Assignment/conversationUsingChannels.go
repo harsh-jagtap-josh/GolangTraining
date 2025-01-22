f061d9299ec20eec08ba9c7d01ea18a892859df8
harsh-jagtap-josh <harsh.jagtap@joshsoftware.com>
2025-01-22 19:36:52 +0530

    add review changes, optimize code, error handling

--- Day 6 Assessment/Assignment/conversationUsingChannels.go
+++ Day 6 Assessment/Assignment/conversationUsingChannels.go
@@ -1,71 +1,77 @@
+// 1. Given a string containing conversation between alice and bob,
+// In the string, if it reaches $, it means end of alice message,
+// and if it reaches #, it means end of bob's message,
+// and if it reaches ^, it means end of conversation ignore string after that.
+
+// Note: given string doesn't contain any spaces. If message contains two continuous conversations from one person it should be printed one after another as given in the example.
+
+// write a program to separate out messages from alice and bob. Write messages from alice and bob on seperate channels, whenever a message from alice/bob, print it in front of their name as shown in the example below:
+
+// Note: there is a single space before and after colon(:) and no space before and after comma.
+
+// e.g: "helloBob$helloalice#howareyou?#Iamgood.howareyou?$^"
+// output: alice : helloBob,bob : helloalice,bob : howareyou?,alice : Iamgood.howareyou?
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
-func main() {
-
-	conversation := "helloBob$helloalice#howareyou?#Iamgood.howareyou?$^"
-
-	var wg sync.WaitGroup
-
-	channelAlice := make(chan string, 10)
-	channelBob := make(chan string, 10)
+func handleConversation(channelAlice chan string, channelBob chan string, conversation string) string {
 
 	temp := ""
 
 	finalStr := ""
 
-	for i := 0; i < len(conversation); i++ {
+	for i, _ := range conversation {
 
 		if conversation[i:i+1] == "$" {
-			// received msg from Alice
-			wg.Add(1)
 
-			// add msg to Alice's channel
-			go addMsgToAlice(channelAlice, temp, &wg)
+			// received msg from Alice
+			channelAlice <- temp
 			temp = ""
-			wg.Wait()
 
 		} else if conversation[i:i+1] == "#" {
-			// received msg from Bob
-			wg.Add(1)
 
-			// add message to Bob's channel
-			go addMsgToBob(channelBob, temp, &wg)
+			// received msg from Bob
+			channelBob <- temp
 			temp = ""
-			wg.Wait()
 
 		} else if conversation[i:i+1] == "^" {
 			break
+
 		} else {
 			temp = temp + conversation[i:i+1]
 		}
 
-		// using Select to check which channel has received a msg...
-		// if msg received, add it to conversation
-
-		select {
-		case m1 := <-channelAlice:
-			finalStr = finalStr + ", alice : " + m1 // check if Alice's channel has a message, add it to our final String
-		case m2 := <-channelBob:
-			finalStr = finalStr + ", bob: " + m2 // check if Alice's channel has a message, add it to our final String
-		default:
-			continue
-		}
 	}
-	fmt.Println(finalStr)
+
+	addMsgToFinalStr(channelAlice, channelBob, finalStr)
+
+	return finalStr
 
 }
 
-func addMsgToAlice(alice chan string, msg string, wg *sync.WaitGroup) {
-	alice <- msg
-	wg.Done()
+func addMsgToFinalStr(channelAlice, channelBob chan string, finalStr string) {
+
+	select {
+	case m1 := <-channelAlice:
+		finalStr = finalStr + ", alice : " + m1 // check if Alice's channel has a message, add it to our final String
+
+	case m2 := <-channelBob:
+		finalStr = finalStr + ", bob: " + m2 // check if Alice's channel has a message, add it to our final String
+
+	default:
+
+	}
 }
 
-func addMsgToBob(bob chan string, msg string, wg *sync.WaitGroup) {
-	bob <- msg
-	wg.Done()
+func main() {
+
+	conversation := "helloBob$helloalice#howareyou?#Iamgood.howareyou?$^"
+
+	channelAlice := make(chan string, 10)
+	channelBob := make(chan string, 10)
+
+	handleConversation(channelAlice, channelBob, conversation)
+
 }
--- Day 6 Assessment/Assignment/reverseString.go
+++ Day 6 Assessment/Assignment/reverseString.go
@@ -1,41 +1,33 @@
+// 2. Given a string, reverse it using one go routine.And inside go routine print reversed string and number of goroutines launched
+
+// e.g: Input: test123 output: 321tset 2
+
 package main
 
 import (
 	"fmt"
-	"sync"
+	"runtime" // contains operations that interact with Go's runtime system
+	"time"
 )
 
-func reverseString(str string) string {
-	newStr := ""
-
-	for i := len(str) - 1; i >= 0; i-- {
-		newStr = newStr + str[i:i+1]
+func reverseString(str *string) {
+	runes := []rune(*str)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
 	}
+	*str = string(runes)
 
-	return newStr
+	goRoutinesLaunched := runtime.NumGoroutine() // defines the number of Go Routines launched in a program
+
+	fmt.Println(*str, goRoutinesLaunched)
+
 }
 
 func main() {
 
 	var inputString string = "test123"
-	var wg sync.WaitGroup
 
-	wg.Add(1)
+	go reverseString(&inputString)
 
-	go func() {
-		defer wg.Done()
-		// Reverse the string
-		inputString = reverseString(inputString)
-	}()
-
-	wg.Wait()
-
-	wg.Add(1)
-
-	go func() {
-		defer wg.Done()
-		fmt.Println(inputString, 2)
-	}()
-
-	wg.Wait()
+	time.Sleep(time.Millisecond * 5) // waiting for all routines to finish
 }
